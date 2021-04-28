70eb5263daad74917238ac445b2331182d7e0ab4
rodages <zanasmakarov@gmail.com>
2021-04-28 11:27:51 +0100

    ready for deploy

---
+++ videosHooks/src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const SearchBar = ({onFormSubmit}) => {
+    const [term, setTerm] = useState('');
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        
+        onFormSubmit(term)
+    }
+
+    return(
+        <div className="search-bar ui segment">
+            <form 
+            className="ui form"
+            onSubmit={onSubmit}
+            >
+                <div className="field">
+                    <label>Video Search</label>
+                    <input 
+                    type="text" 
+                    placeholder="type search term"
+                    value={term}
+                    onChange={(event) => setTerm(event.target.value)}
+                    />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+
+export default SearchBar
